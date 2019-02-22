082b6b5ec08db26fb994cdf4e6f29e4c2baac79c
小黑也叫小白 <834571627@qq.com>
2019-02-22 16:25:21 +0800

    no message

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Redirect} from 'react-router-dom';
+import { HashRouter as Router, Switch, Redirect} from 'react-router-dom';
 import RouterMap from './routerMap'
 import routers from './router';
 
 export default class router extends Component {
   render() {
     return (
-      <Router>
+      <Router >
         <Switch>
           { RouterMap(routers) }
           <Redirect to="/app" />
